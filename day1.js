bffdaed2c7f5d608c6e6baa7e836d8b82db11b59
soonger3306 <soonger3306@daum.net>
2023-03-13 02:06:49 +0900

    day1

---
+++ day1.js
@@ -0,0 +1,21 @@
+// 10172 개
+ // const a = String('|\\_/|\n|q p|   /}\n( 0 )"""\\ \n|"^"`    |\n||_/=\\\\__|');
+ // console.log(a);
+
+// 10171 고양이 
+ // a = String("\\    /\\ \n )  ( ')\n(  /  )\n \\(__)| ")
+ // console.log(a)
+
+// 10869 사칙연산 
+ // const [a,b] = [7,3]
+ // const one = +a
+ // const two = +b
+ // value =(`${one+two}\n${one-two}\n${one*two}\n${Math.floor(one/two)}\n${one%two}`)
+ // console.log(value) 
+
+// 11654 아스키코드  
+
+// const input =["A","C","0","9","a","c"];
+ // for(var i = 0; i <input.length; i++){
+ //    console.log(input[i].charCodeAt(0))
+//   }
