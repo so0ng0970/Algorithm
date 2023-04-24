2176b58a3b02437998836e883e29ac924c2219f8
soonger3306 <soonger3306@daum.net>
2023-04-24 19:16:31 +0900

    leetCode

--- baekjoon/dart/11720_숫자의 합.dart
+++ baekjoon/dart/11720_숫자의 합.dart
@@ -1,10 +1,10 @@
-void main(List<String> args) {
-  List<String> arr = ["54321", "70000", "1", "10987654321"];
+// void main(List<String> args) {
+//   List<String> arr = ["54321", "70000", "1", "10987654321"];
 
-  for (String nums in arr) {
-    List<String> numbers = nums.split("");
-    List<int> num = numbers.map((e) => int.parse(e)).toList();
-    int result = num.reduce((a, b) => (a + b));
-  print(result);
-  }
-}
+//   for (String nums in arr) {
+//     List<String> numbers = nums.split("");
+//     List<int> num = numbers.map((e) => int.parse(e)).toList();
+//     int result = num.reduce((a, b) => (a + b));
+//   print(result);
+//   }
+// }
