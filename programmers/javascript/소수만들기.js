73d797f19b73e94e22205890e689844f92de9b06
soonger3306 <soonger3306@daum.net>
2024-02-29 00:20:19 +0900

    feat:소수만들기

--- programmers/javascript/소수만들기.js
+++ programmers/javascript/소수만들기.js
@@ -14,7 +14,10 @@ function solution(nums) {
 }
 
 function isPrime(num) {
-    for (let i = 2; i < num; i++)
-        if (num % i === 0) return false;
-    return true;
+    for(let i = 2; i <num; i++ ){
+        if(num%i === 0 ){
+            return false;
+        }
+    }
+     return true;
 }
